1edde0c1fe8ada2b2d00dfa636dee1f59bf95e1a
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2026-01-01 19:37:26 -0800

    feat: impl default on id types

--- src/domain/types.rs
+++ src/domain/types.rs
@@ -3,6 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, Serialize, Deserialize)]
 pub struct PlayerId(pub uuid::Uuid);
 
+impl Default for PlayerId {
+    fn default() -> Self {
+        PlayerId::new()
+    }
+}
+
 impl PlayerId {
     pub fn new() -> Self {
         Self(uuid::Uuid::new_v4())
@@ -18,11 +24,23 @@ impl GameId {
     }
 }
 
+impl Default for GameId {
+    fn default() -> Self {
+        GameId::new()
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, Serialize, Deserialize)]
 pub struct LobbyId(pub uuid::Uuid);
 
 impl LobbyId {
     pub fn new() -> Self {
         Self(uuid::Uuid::new_v4())
+    }
+}
+
+impl Default for LobbyId {
+    fn default() -> Self {
+        LobbyId::new()
     }
 }

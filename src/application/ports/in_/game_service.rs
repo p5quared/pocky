60950178994cbc2ddbe02d220222a4b6684eba3c
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2026-01-05 19:58:50 -0800

    feat: launch game from service

--- src/application/ports/in_/game_service.rs
+++ src/application/ports/in_/game_service.rs
@@ -63,32 +63,19 @@ where
         Ok(())
     }
 
-    pub async fn new_game(
+    pub async fn launch_game(
         &mut self,
         players: Vec<PlayerId>,
         starting_balance: i32,
         config: GameConfig,
     ) -> Result<GameId, GameServiceError> {
         let game_id = GameId::new();
-        let game_state = GameState::new(players, starting_balance, config);
+        let (game_state, effects) = GameState::launch(players, starting_balance, config);
+
         self.repository.save_game(game_id, &game_state).await;
-        Ok(game_id)
-    }
-
-    pub async fn start_game(
-        &mut self,
-        game_id: GameId,
-    ) -> Result<(), GameServiceError> {
-        let Some(mut game_state) = self.repository.load_game(game_id).await else {
-            return Err(GameServiceError::GameNotFound(game_id));
-        };
-
-        let effects = game_state.process_action(GameAction::Start)?;
-        self.repository.save_game(game_id, &game_state).await;
-
         self.process_effects(game_id, effects).await;
 
-        Ok(())
+        Ok(game_id)
     }
 
     pub async fn process_price_tick(

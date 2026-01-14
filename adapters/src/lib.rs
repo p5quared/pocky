61c2ef422ab8644ddc98072d12d85978a18e051f
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2026-01-13 23:14:34 -0800

    feat: remove old interfaces

--- adapters/src/lib.rs
+++ adapters/src/lib.rs
@@ -1,5 +1,3 @@
-mod tokio_timer;
 mod websocket;
 
-pub use tokio_timer::TokioTimer;
 pub use websocket::{AppState, IncomingMessage, WebSocketNotifier, handle_connection};
--- application/src/ports/out_/mod.rs
+++ application/src/ports/out_/mod.rs
@@ -1,7 +1,5 @@
-mod common;
 mod game;
 mod queue;
 
-pub use common::AsyncTimer;
 pub use game::{GameEventNotifier, GameNotification, GameServiceError};
 pub use queue::QueueNotifier;
